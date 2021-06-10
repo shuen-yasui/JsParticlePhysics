68f557765e85ed28b486c3302814f945ed607fd4
Shuen Yasui <shuenyasui@gmail.com>
2021-06-09 22:29:34 -0700

    moved all parameters except position and r out of Particle. Added alpha to line color. Made particle fade over time. Kept particle stationary until faded.

--- jsParticlePhysics.js
+++ jsParticlePhysics.js
@@ -1,35 +1,37 @@
 class Particle {
-	constructor(pos){
-		this.x=0;
-		this.y=0;
-		this.centerX=0;
-		this.centerY=0;
-		this.velocity=0.02;
-		this.pathRadius=50;
-		this.posInRadians = pos;
+	constructor(x,y){
+		this.x=x;
+		this.y=y;
 		this.radius=5;
+		this.alpha=1;
 	}
 	update(){
-		// Update Positions
-		this.posInRadians += this.velocity;
-		this.x = this.centerX + (this.pathRadius * (Math.cos(this.posInRadians)));
-		this.y = this.centerY + (this.pathRadius * (Math.sin(this.posInRadians)));
-		this.centerX=mouseX;
-		this.centerY=mouseY;
-    c.beginPath();
+		c.beginPath();
     c.arc(this.x,this.y,this.radius,0,Math.PI * 2,false);
-    c.strokeStyle = "white";
+    c.strokeStyle = 'rgba(255, 255, 255, '+this.alpha+')';
     c.stroke();
     c.fillStyle = "black";
     c.fill();
     c.closePath();
+		this.alpha-=0.01;
 	}
 }
 function update(){
   c.clearRect(0,0,window.innerWidth,window.innerHeight);
+	posRadians+=0;
+	for(var i=0;i<particleCnt;i++){
+		thisRadians=((i/(particleCnt))*Math.PI*2);
+		var x=mouseX + (pathRadius * (Math.cos(thisRadians)));
+		var y=mouseY + (pathRadius * (Math.sin(thisRadians)));
+	  particles.push(new Particle(x,y));
+	}
   i=0;
   while(i<particles.length){
     particles[i].update();
+		if(particles[i].alpha<0){
+			particles.splice(i,1);
+			continue;
+		}
     i+=1;
   }
 	requestAnimationFrame(update);
@@ -47,11 +49,11 @@ function mouseMove(mouseEvent){
 var mouseX=0;
 var mouseY=0;
 var particles=[];
-var particleCnt=10;
-for(var i=0;i<particleCnt;i++){
-  particles.push(new Particle((i/(particleCnt))*Math.PI*2));
-}
+var particleCnt=5;
 var canvas = document.querySelector('canvas');
+var velocity=0.02;
+var pathRadius=50;
+var posRadians=1;
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 var c = canvas.getContext('2d');
