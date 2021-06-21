da246bf1d1499c972f8ebd7d8f2e1df56a47d4ef
Shuen Yasui <shuenyasui@gmail.com>
2021-06-20 17:32:12 -0700

    Add comments. Renamed variables

--- jsParticlePhysics.js
+++ jsParticlePhysics.js
@@ -6,6 +6,7 @@ class Particle {
 		this.alpha=1;
 	}
 	update(){
+		// Draw new particle
 		c.beginPath();
     c.arc(this.x,this.y,this.radius,0,Math.PI * 2,false);
     c.strokeStyle = 'rgba(255, 255, 255, '+this.alpha+')';
@@ -13,31 +14,40 @@ class Particle {
     c.fillStyle = "black";
     c.fill();
     c.closePath();
-		this.alpha-=0.01;
+		// Speed of particle fades out
+		this.alpha-=0.015;
 	}
 }
 function update(){
+	// Clear screen
   c.clearRect(0,0,window.innerWidth,window.innerHeight);
-	posRadians+=0.01;
+	// Speed particle rotates around mouse
+	posRadians+=0.017;
+	// Create particleCnt number of particles
 	for(var i=0;i<particleCnt;i++){
-		var thisRadians=((i/(particleCnt))*Math.PI*2);
+		var radianPositionInOrbit=((i/(particleCnt))*Math.PI*2);
 		var delta_x = centerParticleX - mouseX;
 		var delta_y = centerParticleY - mouseY;
-		var theta_radians = Math.atan2(delta_y, delta_x);
-		var hypOri = Math.hypot(delta_x,delta_y);
-		var hypNew = 0;
-		if(hypOri>1){
-			hypNew=hypOri*0.995;
+		var radiansOfDelta = Math.atan2(delta_y, delta_x);
+		var hypotOfDelta = Math.hypot(delta_x,delta_y);
+		var hypotToMoveTo = 0;
+		// Caluclate Move distance towards mouse, % of distance
+		if(hypotOfDelta>1){
+			hypotToMoveTo=hypotOfDelta*0.995;
 		}
-		centerParticleY = (hypNew*Math.sin(theta_radians))+mouseY;
-		centerParticleX = (hypNew*Math.cos(theta_radians))+mouseX;
-		var x=centerParticleX + (pathRadius * (Math.cos(thisRadians+posRadians)));
-		var y=centerParticleY + (pathRadius * (Math.sin(thisRadians+posRadians)));
+		// Calculate x and y center of orbit
+		centerParticleY = (hypotToMoveTo*Math.sin(radiansOfDelta))+mouseY;
+		centerParticleX = (hypotToMoveTo*Math.cos(radiansOfDelta))+mouseX;
+		// Calculate x and y of particle
+		var x=centerParticleX + (pathRadius * (Math.cos(radianPositionInOrbit+posRadians)));
+		var y=centerParticleY + (pathRadius * (Math.sin(radianPositionInOrbit+posRadians)));
 	  particles.push(new Particle(x,y));
 	}
   i=0;
+	// Update all particles
   while(i<particles.length){
     particles[i].update();
+		// Remove particle if invisible
 		if(particles[i].alpha<0){
 			particles.splice(i,1);
 			continue;
@@ -61,11 +71,11 @@ var mouseY=0;
 var particles=[];
 var particleCnt=5;
 var canvas = document.querySelector('canvas');
-var velocity=0.02;
 var pathRadius=50;
 var posRadians=1;
 var centerParticleX = 0;
 var centerParticleY = 0;
+// Initialize Canvas
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 var c = canvas.getContext('2d');
